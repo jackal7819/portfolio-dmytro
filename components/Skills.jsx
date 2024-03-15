c32d8a6ef976025f5e5956d0956c71e3b9ce9eb6
Viktor Filippov <jackal3166@gmail.com>
2024-03-15 19:14:50 +0200

    Change skills

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -3,34 +3,40 @@
 import { motion, useInView } from 'framer-motion';
 import { useRef } from 'react';
 
-import ScrollSvg from './ScrollSvg'
+import ScrollSvg from './ScrollSvg';
 
 const skills = [
-	{ id: 1, name: 'JavaScript' },
-	{ id: 2, name: 'TypeScript' },
-	{ id: 3, name: 'React' },
-	{ id: 4, name: 'React Native' },
-	{ id: 5, name: 'Next.js' },
-	{ id: 6, name: 'Vue' },
-	{ id: 7, name: 'Nuxt' },
-	{ id: 8, name: 'HTML' },
-	{ id: 9, name: 'CSS' },
-	{ id: 10, name: 'SCSS' },
-	{ id: 11, name: 'Tailwind' },
-	{ id: 12, name: 'Bootstrap' },
-	{ id: 13, name: 'Material UI' },
-	{ id: 14, name: 'Git' },
-	{ id: 15, name: 'GitHub' },
-	{ id: 16, name: 'Rest API' },
-	{ id: 17, name: 'GraphQL' },
-	{ id: 18, name: 'Apollo' },
-	{ id: 19, name: 'Redux Toolkit' },
-	{ id: 20, name: 'Zustand' },
-	{ id: 21, name: 'Pinia' },
-	{ id: 22, name: 'Framer Motion' },
-	{ id: 23, name: 'Firebase' },
-	{ id: 24, name: 'Vite' },
-	{ id: 25, name: 'Storybook' },
+	{
+		id: 1,
+		name: 'In-depth knowledge in repair and maintenance of metallurgical equipment',
+	},
+	{ id: 2, name: 'Experience working with complex mechanisms and systems' },
+	{
+		id: 3,
+		name: 'Ability to read drawings, diagrams, and technical documentation',
+	},
+	{
+		id: 4,
+		name: 'Knowledge of the principles of hydraulic and pneumatic systems',
+	},
+	{ id: 5, name: 'Experience with hand and power tools' },
+	{
+		id: 6,
+		name: 'Experience in installation, disassembly, and adjustment of equipment',
+	},
+	{
+		id: 7,
+		name: 'Ability to work with measuring instruments and diagnostic equipment',
+	},
+	{ id: 8, name: 'Knowledge of safety standards for repair work' },
+	{
+		id: 9,
+		name: 'Attention to detail, meticulousness, and ability to concentrate',
+	},
+	{
+		id: 10,
+		name: 'Responsibility and commitment to continuous professional skill development',
+	},
 ];
 
 const Skills = () => {
@@ -48,11 +54,11 @@ const Skills = () => {
 				Skills
 			</motion.h1>
 			{/* SKILLS LIST */}
-			<div className='grid grid-cols-2 gap-4 sm:grid-cols-3 md:grid-cols-4 2xl:grid-cols-5 auto-rows-fr'>
+			<div className='grid grid-cols-1 gap-4'>
 				{skills.map((skill) => (
 					<div
 						key={skill.id}
-						className='flex items-center justify-center p-2 text-sm text-center text-white duration-300 bg-black rounded cursor-pointer hover:bg-amber-500'
+						className='flex items-center justify-start p-2 text-sm text-white duration-300 bg-black rounded cursor-pointer hover:bg-amber-500'
 					>
 						{skill.name}
 					</div>
